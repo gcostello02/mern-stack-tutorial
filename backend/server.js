9e6a9150ba146eaffebec40ad90d1917551b7e46
gcostello02 <GRANT.COSTELLO.02@gmail.com>
2023-11-18 11:26:39 -0500

    Routes

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,11 @@ const express = require('express')
 // EXPRESS APP
 const app = express()
 
+// ROUTES
+app.get('/', (req, res) => {
+    res.json({mssg: 'Welcome to the app'})
+})
+
 // LISTEN FOR REQUESTS
 app.listen(4000, () => {
     console.log('listening on port 4000')
