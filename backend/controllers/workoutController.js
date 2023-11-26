e1ef3aa2d582c6224eb7c163ae6cdb40d43f572b
gcostello02 <GRANT.COSTELLO.02@gmail.com>
2023-11-26 15:33:44 -0500

    update workout

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -15,7 +15,7 @@ const getWorkout = async (req, res) => {
     }
     const workout = await Workout.findById(id)
     if(!workout) {
-        return res.status(404).json({error: 'No such workout'})
+        return res.status(400).json({error: 'No such workout'})
     }
     res.status(200).json(workout)
 }
@@ -39,16 +39,30 @@ const deleteWorkout = async (req, res) => {
     }
     const workout = await Workout.findOneAndDelete({_id: id})
     if(!workout) {
-        return res.status(404).json({error: 'No such workout'})
+        return res.status(400).json({error: 'No such workout'})
     }
     res.status(200).json(workout)
 }
 
 // UPDATE A WORKOUT
+const updateWorkout = async (req, res) => {
+    const { id } = req.params
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({error: 'No such workout'})
+    }
+    const workout = await Workout.findByIdAndUpdate({_id: id}, {
+        ...req.body
+    })
+    if(!workout) {
+        return res.status(400).json({error: 'No such workout'})
+    }
+    res.status(200).json(workout)
+}
 
 module.exports = {
     getWorkouts,
     getWorkout,
     createWorkout,
-    deleteWorkout
+    deleteWorkout,
+    updateWorkout
 }
