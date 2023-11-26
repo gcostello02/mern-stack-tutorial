62fe649baf843752384351fb90ab80b5d2baa871
gcostello02 <GRANT.COSTELLO.02@gmail.com>
2023-11-26 15:26:07 -0500

    delete workout

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -32,11 +32,23 @@ const createWorkout = async (req, res) => {
 }
 
 // DELETE A WORKOUT
+const deleteWorkout = async (req, res) => {
+    const { id } = req.params
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({error: 'No such workout'})
+    }
+    const workout = await Workout.findOneAndDelete({_id: id})
+    if(!workout) {
+        return res.status(404).json({error: 'No such workout'})
+    }
+    res.status(200).json(workout)
+}
 
 // UPDATE A WORKOUT
 
 module.exports = {
     getWorkouts,
     getWorkout,
-    createWorkout
+    createWorkout,
+    deleteWorkout
 }
