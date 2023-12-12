ea36da9c76b5d7bf2d9a08424ef7b7a7e9f1a444
gcostello02 <GRANT.COSTELLO.02@gmail.com>
2023-12-11 20:08:16 -0500

    Fixed typo

--- frontend/src/components/WorkoutForm.js
+++ frontend/src/components/WorkoutForm.js
@@ -9,7 +9,7 @@ const WorkoutForm = () => {
     const handleSubmit = async (e) => {
         e.preventDefault()
         const workout = {title, load, reps}
-        const response = await fetch('/api/workout', {
+        const response = await fetch('/api/workouts', {
             method: 'POST',
             body: JSON.stringify(workout),
             headers: {
