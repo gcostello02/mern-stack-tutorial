c3f3f0ccb11f2645b6efa7985a2e46329e30883e
gcostello02 <GRANT.COSTELLO.02@gmail.com>
2023-12-17 21:42:06 -0500

    Finishing Touches

--- frontend/src/components/WorkoutDetails.js
+++ frontend/src/components/WorkoutDetails.js
@@ -1,5 +1,8 @@
 import { useWorkoutContext } from '../hooks/useWorkoutsContext'
 
+// DATE FNS
+import formatDistanceToNow from 'date-fns/formatDistanceToNow'
+
 const WorkoutDetails = ({ workout }) => {
     const { dispatch } = useWorkoutContext()
     const handleClick = async () => {
@@ -17,7 +20,7 @@ const WorkoutDetails = ({ workout }) => {
             <h4>{workout.title}</h4>
             <p><strong>Load (kg): </strong>{workout.load}</p>
             <p><strong>Reps: </strong>{workout.reps}</p>
-            <p>{workout.createdAt}</p>
+            <p>{formatDistanceToNow(new Date(workout.createdAt), { addSuffix: true })}</p>
             <span className="material-symbols-outlined" onClick={handleClick}>delete</span>
         </div>
     )
